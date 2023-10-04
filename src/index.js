95f98cc1268800825d2bcf10f173e88bec0e2cae
cross-sniper <137736431+cross-sniper@users.noreply.github.com>
2023-10-04 15:21:42 -0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 //this script was generate automaticaly, do not modify it manualy, rebuild the code after changing something
 
-div = document.createElement("div");div.id = "/void";div.innerHTML = `<h1><br>attention, this game is still beeing worked on<br></h1><br><br>this game, as well as the "engine" is still beeing worked on, expect changes, and the game to stop working<br><br>if you want to help me make this game, as well as the engine, be somewhat decent<br><br><a href="https://github.com/cross-sniper/project-neo-dx">here is the git repo</a>`;document.getElementById("game-container").appendChild(div);
+div = document.createElement("div");div.id = "/void";div.innerHTML = `<h1><br>attention, this game is still beeing worked on<br></h1><br><br>this game, as well as the "engine" is still beeing worked on, expect changes, and the game to stop working<br><br>if you want to help me make this game, as well as the engine, be somewhat decent<br><br><a href="https://github.com/cross-sniper/project-neo-dx">here is the git repo</a><br><br>[[start playinig|/start]]`;document.getElementById("game-container").appendChild(div);
 
 div = document.createElement("div");div.id = "/start";div.innerHTML = ``;document.getElementById("game-container").appendChild(div);
-
-div = document.createElement("div");div.id = "/console";div.innerHTML = ``;document.getElementById("game-container").appendChild(div);
